27df2b6e7350d7406c02761b8224dc3a8ffaae3f
cristina <yosola@gmail.com>
2018-03-07 22:59:38 -0800

    Numbers Activity working in english

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -1,7 +1,12 @@
 package com.example.android.miwok;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -9,5 +14,20 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        ArrayList<String> wordsOfNumbers = new ArrayList<>(Arrays.asList("One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten"));
+
+        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
+
+        for(int index = 0; index < wordsOfNumbers.size(); index++){
+            TextView wordView = new TextView(this);
+            wordView.setText(wordsOfNumbers.get(index));
+            rootView.addView(wordView);
+
+        }
+
+
     }
-}
+
+    }
+
